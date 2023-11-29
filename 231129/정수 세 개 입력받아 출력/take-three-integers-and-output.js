dc121d5fdcb6bc12a8245533a805ee384d104cae
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-11-29 11:08:34 +0900

    Title: 정수 세 개 입력받아 출력, Time: 219ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 231129/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split("\n");
+
+let arr = input[0].split(" ")
+let a = Number(arr[0]);
+let b = Number(arr[1]);
+let c = Number(input[1]);
+
+console.log(a,b,c);
