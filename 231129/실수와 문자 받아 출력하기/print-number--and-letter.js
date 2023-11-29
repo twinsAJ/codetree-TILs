05dc4ed74c8c93a8b49bcf6abac7bd5309f14792
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-11-29 11:56:30 +0900

    Title: 실수와 문자 받아 출력하기, Time: 288ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 231129/실수와 문자 받아 출력하기/print-number--and-letter.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split("\n");
+let c = input[0];
+let a = Number(input[1]);
+let b = Number(input[2]);
+console.log(c);
+console.log(a.toFixed(2));
+console.log(b.toFixed(2));
