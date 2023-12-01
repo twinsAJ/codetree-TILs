82e57809b4454a22676b695e638cb52a624bb04c
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-12-01 11:52:35 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 253ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 231201/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+a += 8
+b += 14
+
+console.log(a);
+console.log(b);
+console.log(a*b);
