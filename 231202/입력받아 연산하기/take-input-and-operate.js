5cb7a24588063aba838eb03c402a36adabe80781
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-12-02 16:46:31 +0900

    Title: 입력받아 연산하기, Time: 315ms, Memory: 45MB, Status: Passed - CodeTree

---
+++ 231202/입력받아 연산하기/take-input-and-operate.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let a = Number(input[0]);
+let b = Number(input[1]);
+a += 87
+b %= 10
+console.log(a);
+console.log(b);
