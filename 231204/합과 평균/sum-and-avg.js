de06c99758e12917034d282c087d1530fcc6db4f
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-12-04 18:32:14 +0900

    Title: 합과 평균, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231204/합과 평균/sum-and-avg.js
+++ 231204/합과 평균/sum-and-avg.js
@@ -3,4 +3,4 @@ let input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]);
 let b = Number(input[1]);
 
-console.log(a+b,(a+b)/2);
+console.log(a+b,((a+b)/2)).toFixed(1);
