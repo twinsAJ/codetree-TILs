4c165e3dfeb8d75192233c2db3470ac016cfdb59
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-12-04 18:36:13 +0900

    Title: 합과 평균, Time: 219ms, Memory: 13MB, Status: Passed - CodeTree

--- 231204/합과 평균/sum-and-avg.js
+++ 231204/합과 평균/sum-and-avg.js
@@ -2,5 +2,5 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]);
 let b = Number(input[1]);
-
-console.log(a+b,((a+b)/2)).toFixed(1);
+c = a+b
+console.log(c,(c/2).toFixed(1));
