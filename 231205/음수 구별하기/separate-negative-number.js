45a591891e131b25d07ac1cbcdecbf7c13ec0b81
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-12-05 12:28:36 +0900

    Title: 음수 구별하기, Time: 209ms, Memory: 13MB, Status: Passed - CodeTree

--- 231205/음수 구별하기/separate-negative-number.js
+++ 231205/음수 구별하기/separate-negative-number.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
 let n = Number(input[0]);
 
-if (n<0){
-    console.log(n);
+console.log(n);
+
+if ( n < 0 ) {
     console.log("minus");
 }
