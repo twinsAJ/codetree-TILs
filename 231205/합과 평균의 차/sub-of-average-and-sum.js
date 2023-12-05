1a9a008c10b95b313a7f56b4a3abaf8066a2d1a8
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-12-05 11:58:31 +0900

    Title: 합과 평균의 차, Time: 279ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231205/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+console.log(a+b+c);
+console.log(parseInt((a+b+c)/3));
+console.log((a+b+c)-parseInt((a+b+c)/3));
