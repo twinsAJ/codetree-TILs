8678fe5c2cc84763a435205984f0a1a1d7851a24
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-12-09 12:25:58 +0900

    Title: 출석 부르기, Time: 226ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231209/출석 부르기/calling-attendance.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString();
+
+if (n=1){
+    console.log("John");
+}
+else if(n=2){
+    console.log("Tom");
+}
+else if(n=3){
+    console.log("Paul");
+}
+else{
+    console.log("Vacancy");
+}
