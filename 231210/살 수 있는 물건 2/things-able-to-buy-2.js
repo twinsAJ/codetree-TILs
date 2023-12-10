aad3e75870258666cd8a6913c9803c363a031d55
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-12-10 15:21:54 +0900

    Title: 살 수 있는 물건 2, Time: 239ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231210/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString();
+if (n>=3000) {
+    console.log("book");
+}
+else if (n>=1000) {
+    console.log("mask");
+}
+else if (n>=500) {
+    console.log("pen");
+}
+else {
+    console.log("no");
+}
