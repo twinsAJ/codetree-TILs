c04822aba24b3da149b3598bd53bbcafdd5ea9b0
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-12-11 09:37:36 +0900

    Title: 최대 2번의 연산, Time: 210ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 231211/최대 2번의 연산/up-to-2-calculations.js
+++ 231211/최대 2번의 연산/up-to-2-calculations.js
@@ -3,10 +3,8 @@ let a = Number(fs.readFileSync(0).toString().trim());
 
 if ((a%2) == 0) {
     b = a/2
-    if (b%2 == 1) {
-        console.log((b+1)/2);
-    }
+    console.log((b+1)/2);
 }
-if (a%2 == 1) {
+else{
     console.log((a+1)/2);
 }
