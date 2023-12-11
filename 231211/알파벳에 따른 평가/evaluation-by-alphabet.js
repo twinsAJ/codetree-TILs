fd800568cf0375fa7d014403381434034d2a051f
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2023-12-11 09:14:10 +0900

    Title: 알파벳에 따른 평가, Time: 234ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231211/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -0,0 +1,20 @@
+let fs = require("fs");
+let n = fs.readFileSync(0).toString();
+if (n === "S") {
+    console.log("Superior");
+}
+else if (n === "A") {
+    console.log("Excellent");
+}
+else if (n === "B") {
+    console.log("Good");
+}
+else if (n === "C") {
+    console.log("Usually");
+}
+else if (n === "D") {
+    console.log("Effort");
+}
+else {
+    console.log("Failure");
+}
