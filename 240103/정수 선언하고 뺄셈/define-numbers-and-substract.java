d47de247db815d4a5a3c6d32370b3c5ca6b708e2
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2024-01-03 14:26:42 +0900

    Title: 정수 선언하고 뺄셈, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        a = 97;
+        int b;
+        b = 13;
+        int c = a-b;
+        System.out.print(a+" - "+b+" = "+c);
+    }
+}
