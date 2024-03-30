10b2f424d99e4d6f4bf7f7547a21d73f8dc5207f
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2024-03-30 23:27:39 +0900

    Title: 달에서 무게 구하기, Time: 112ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240330/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 13;
+        double b = 0.165;
+        double c = a * b;
+        System.out.printf(a + " * ");
+        System.out.printf("%.6f", b);
+        System.out.print(" = ");
+        System.out.printf("%.6f", c);
+    }
+}
