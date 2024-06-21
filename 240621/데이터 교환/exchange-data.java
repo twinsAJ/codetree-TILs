ae1ddca030649ceaf0a376cf347a017d36a27433
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2024-06-21 21:55:38 +0900

    Title: 데이터 교환, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240621/데이터 교환/exchange-data.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 5, b = 6, c = 7;
+        int temp;
+
+        temp = b;
+        b = a;
+        a = c;
+        c = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.print(c);
+    }
+}
