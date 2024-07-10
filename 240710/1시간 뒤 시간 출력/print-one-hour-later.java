7a99ce428f83ecd3e432fe00f9c5f88b21ad8461
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2024-07-10 13:09:30 +0900

    Title: 1시간 뒤 시간 출력, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240710/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter(":");
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.print(h+1 + ":" + m);
+    }
+}
