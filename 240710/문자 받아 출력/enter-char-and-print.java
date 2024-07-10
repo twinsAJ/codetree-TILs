5395bf5ba347195710e5fd2ec721ea54f1b5104c
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2024-07-10 11:55:31 +0900

    Title: 문자 받아 출력, Time: 111ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240710/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char c = a.charAt(0);
+
+        System.out.print(c);
+    }
+}
