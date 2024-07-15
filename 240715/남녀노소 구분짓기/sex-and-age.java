f95e96ab07bb453b91fb608177c5f53bfaaad088
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2024-07-15 10:58:23 +0900

    Title: 남녀노소 구분짓기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240715/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(a==0){
+            if(b>=19){
+                System.out.print("MAN");
+            }
+            else{
+                System.out.print("BOY");
+            }
+        }
+        if(a==1){
+            if(b>=19){
+                System.out.print("WOMAN");
+            }
+            else{
+                System.out.print("GIRL");
+            }
+        }
+    }
+}
