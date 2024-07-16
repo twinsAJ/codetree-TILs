f99ed5a6e7cb4997479ef04c0543f7b59fb44d9c
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2024-07-16 11:22:17 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240716/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next(),charAt();
+        int aa = sc.nextInt();
+        char b = sc.next(),charAt();
+        int bb = sc.nextInt();
+        char c = sc.next(),charAt();
+        int cc = sc.nextInt();
+
+        if()
+    }
+}
