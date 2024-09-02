6fff1a9bbc6dc310e87653cbeafc72f88b34aed1
twinsAJ <151008192+twinsAJ@users.noreply.github.com>
2024-09-02 10:44:04 +0900

    Title: 차례로 출력, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240902/차례로 출력/print-in-order.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 17;
+        for(int i = a; i <= b; i++){
+            System.out.print(i + " ");
+        }
+        // 여기에 코드를 작성해주세요.
+    }
+}
